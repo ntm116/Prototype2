f0ebdf98c371808e78785b9109b6b47c82dd4b85
ntm116 <manhnt@metacity.email>
2022-10-18 18:14:41 +0700

    P2-2: Challenge

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    private float timer = 1f;
+    private float delay = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer < delay * 2) 
+            timer += Time.deltaTime;
+        // On spacebar press, send dog
+        if (timer > delay && Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            timer = 0f;
+        }
+    }
+}
